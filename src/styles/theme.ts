7ba5bbe1800d1c9ab4a49b5cfea0d480ae8eccd8
Fernando Martínez <fernando_m1911a1@hotmail.com>
2025-08-26 21:54:40 -0600

    Creates simple theme with light and dark variants.

---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+import { blue, yellow } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+	colorSchemes: {
+		dark: {
+			palette: {
+				primary: {
+					main: yellow[700],
+				},
+				secondary: {
+					main: blue[500],
+				},
+				background: {
+					default: blue[900],
+				},
+			},
+		},
+		light: {
+			palette: {
+				primary: {
+					main: blue[500],
+				},
+				secondary: {
+					main: yellow[700],
+				},
+				background: {
+					default: blue[100],
+				},
+			},
+		},
+	},
+});
