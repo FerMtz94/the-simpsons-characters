05536b80cd6bcc993a694f4455955c04c0ec965f
Fernando Martínez <fernando_m1911a1@hotmail.com>
2025-08-26 21:55:23 -0600

    Provides theme in app entrypoint file.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import { ThemeProvider } from "@mui/material/styles";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import { theme } from "./styles/theme.ts";
+import "./styles/index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -11,8 +13,11 @@ const rootElement = document.getElementById("root");
 if (!rootElement) {
 	throw new Error("Root element not found");
 }
+
 createRoot(rootElement).render(
 	<StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</StrictMode>,
 );
