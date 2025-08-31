0c73e611d50d3068f160b708939cf08adb386e2b
Fernando Martínez <fernando_m1911a1@hotmail.com>
2025-08-31 01:10:29 -0600

    Adds types folder with character interface.

---
+++ src/types/character.ts
@@ -0,0 +1,11 @@
+export interface Character {
+  id: number
+  age: number
+  birthdate: string
+  gender: string
+  name: string
+  occupation: string
+  portraitPath: string
+  phrases: string[]
+  status: string
+}
