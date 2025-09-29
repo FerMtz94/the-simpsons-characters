c3cc9cd3b5c87de92fe9c8f3e668bc29f593d6f1
Fernando Martínez <fernando_m1911a1@hotmail.com>
2025-09-28 22:19:29 -0600

    Uses generic get data method with specified parameters.

--- src/api/characters.ts
+++ src/api/characters.ts
@@ -1,14 +1,9 @@
+import { getData } from "./getData";
+
 export const getCharacters = async () => {
-	let characters = [];
-	try {
-		const response = await fetch("https://thesimpsonsapi.com/api/characters");
-		if (!response.ok) {
-			throw new Error("Couldn't fetch characters");
-		}
-		const data = await response.json();
-		characters = data;
-		return characters;
-	} catch (error) {
-		console.error("Error fetching characters:", error);
-	}
+	return await getData({
+		url: "https://thesimpsonsapi.com/api/characters",
+		fetchErrorMessage: "Couldn't fetch characters",
+		tryCatchError: "Error fetching characters:",
+	});
 };
