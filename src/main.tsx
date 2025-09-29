f956c16d61ff511d800e0d23542ed369c6b5b885
Fernando Martínez <fernando_m1911a1@hotmail.com>
2025-09-28 22:32:16 -0600

    Uses analytics React component

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-// biome-ignore lint/correctness/noUnusedImports: <explanation>
 import { Analytics } from "@vercel/analytics/next";
 
 const rootElement = document.getElementById("root");
@@ -24,5 +23,6 @@ createRoot(rootElement).render(
 				<App />
 			</QueryClientProvider>
 		</ThemeProvider>
+		<Analytics />
 	</StrictMode>,
 );
