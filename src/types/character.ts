0ade37f2aa1bebe32c0daff35239a17921654137
Fernando Martínez <fernando_m1911a1@hotmail.com>
2025-09-28 22:18:20 -0600

    Changes portrait path property name to match with object schema.

--- src/types/character.ts
+++ src/types/character.ts
@@ -1,11 +1,11 @@
 export interface Character {
-  id: number
-  age: number
-  birthdate: string
-  gender: string
-  name: string
-  occupation: string
-  portraitPath: string
-  phrases: string[]
-  status: string
+	id: number;
+	age: number;
+	birthdate: string;
+	gender: string;
+	name: string;
+	occupation: string;
+	portrait_path: string;
+	phrases: string[];
+	status: string;
 }
