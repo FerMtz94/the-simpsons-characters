6fdac9499e869ca5f3db6f0f0d68098c644ceaea
Fernando Martínez <fernando_m1911a1@hotmail.com>
2025-10-15 22:46:41 -0600

    Creates hook to manage characters state

---
+++ src/hooks/useCharacters.ts
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { getCharacters } from "../api/characters";
+import type { Character } from "../types/character";
+
+export const useCharacters = () => {
+	const [characters, setCharacters] = useState<Character[]>([]);
+	const [filteredCharacters, setFilteredCharacters] = useState<Character[]>([]);
+	const [page, setPage] = useState(1);
+
+	const { data: characterData, isLoading } = useQuery({
+		queryKey: ["characters", page],
+		queryFn: () => getCharacters(page.toString()),
+	});
+
+	useEffect(() => {
+		if (!characterData) return;
+		setCharacters(characterData.results);
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}, [characterData]);
+
+	useEffect(() => {
+		setFilteredCharacters(characters);
+	}, [characters]);
+
+	return {
+		characters,
+		filteredCharacters,
+		setFilteredCharacters,
+		page,
+		setPage,
+		isLoading,
+	};
+};
