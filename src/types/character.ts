0d71713578bd08550a77ce4112c91c086f86d016
Fernando Martínez <fernando_m1911a1@hotmail.com>
2025-10-17 01:02:28 -0600

    Adds character detail interface.

--- src/types/character.ts
+++ src/types/character.ts
@@ -9,3 +9,33 @@ export interface Character {
 	phrases: string[];
 	status: string;
 }
+
+export interface CharacterDetail extends Character {
+  description: string;
+  first_appearance_ep_id: number;
+  first_appearance_sh_id: number;
+  first_appearance_ep: FirstAppearanceEp;
+  first_appearance_sh: FirstAppearanceSh;
+}
+
+export interface FirstAppearanceEp {
+  id: number;
+  airdate: string;
+  description: string;
+  episode_number: number;
+  image_path: string;
+  name: string;
+  season: number;
+  synopsis: string;
+}
+
+export interface FirstAppearanceSh {
+  id: number;
+  airdate: string;
+  description: string;
+  episode_number: number;
+  image_path: string;
+  name: string;
+  season: number;
+  synopsis: string;
+}
